c5aa19c8470eaab318f90fd0a80f3ea0e417fb7a
profugolotti <profugolotti@gmail.com>
2025-08-20 00:09:56 +0200

    new file:   01_HelloWorld/hello.c

---
+++ 01_HelloWorld/hello.c
@@ -0,0 +1,15 @@
+/*
+ * File: hello.c
+ * Description: Programma di esempio "Hello, World!" in C.
+ * Author: prof. ugolotti
+ * Date: 19/08/25
+ */
+
+#include <stdio.h>  // Rende disponibili le funzioni della libreria standard di input/output
+
+int main(void) {//Entry point del programma
+    
+    printf("Hello, World!\n");// Stampa "Hello, World!" sulla console
+
+    return 0; // Restituisce 0 per indicare che il programma è terminato correttamente
+}
