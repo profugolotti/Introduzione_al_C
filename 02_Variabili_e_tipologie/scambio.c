769d7bb3096d13cfc79f4c0fd3ee67739d2c8799
profugolotti <profugolotti@gmail.com>
2025-09-03 11:06:00 +0200

    .

--- 02_Variabili_e_tipologie/scambio.c
+++ 02_Variabili_e_tipologie/scambio.c
@@ -1,6 +1,28 @@
+/*
+Esempio di scambio di valori tra due variabili
+
+*/
+
+
 #include <stdio.h>
 
 int main() {
+
+    int a = 3;
+    int b = 7;à
+
+    //leggi le variabili da tastiera
+
+    //Stampo le variabili prima dello scambio
+    printf("le variabili prima dello scambio: a=%d, b=%d\n", a, b);  //3 e 7
+
+
+    // scambio dei valori
+
+
+
+    //Stampo le variabili dopo lo scambio
+    printf("le variabili dopo lo scambio: a=%d, b=%d\n", a, b);  //7 e 3
 
 
     return 0;
